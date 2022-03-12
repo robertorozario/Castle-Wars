e31c827bbe45be83adc673d2272f386d6859309c
Gian Ortiz <brksgian@gmail.com>
2022-03-11 21:36:31 -0300

    Implement logic for player actor interface

--- interface_jogador.py
+++ interface_jogador.py
@@ -3,27 +3,33 @@ import pygame
 from jogador import Jogador
 from cenario import Cenario
 
+
 class InterfaceJogador:
-    
-    def __init__(self, screen: pygame.Surface, jogador: Jogador, cenario: Cenario):
+    """
+    InterfaceJogador representa a interface do ator Jogador com o jogo.
+    """
+
+    def __init__(
+            self, screen: pygame.Surface, jogador: Jogador, cenario: Cenario):
         self.__screen = screen
         self.__jogador = jogador
         self.__cenario = cenario
 
-    def descartar_carta(indice: int):
-        pass
+    def jogar_carta(self, indice: int):
+        self.__jogador.jogar_carta(indice)
+
+    def descartar_carta(self, indice: int):
+        self.__jogador.descarta_carta(indice)
+        if self.__jogador.descartou_max_cartas():
+            self.__cenario.passa_turno_atual_jogador()
 
     def iniciar_jogo(self):
-        pass
+        self.__cenario.iniciar_jogo()
 
     def passar_turno(self):
-        pass
-
-    def adiciona_carta(self, tipo: str, copias: int):
-        pass
-
-    def remove_carta(self, tipo: str, copias: int):
-        pass
+        self.__cenario.passa_turno_atual_jogador()
 
     def seleciona_baralho(self, indice: int):
-        pass
+        baralho = self.__cenario.baralhos_padrao[indice]
+        baralho_copia = baralho.copia()
+        self.__jogador.baralho = baralho_copia
